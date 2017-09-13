0646a09fea7772f06f13f076b7a3ec8c67ebe4b8
Jack Tuck <jack-tuck@hotmail.com>
2017-09-14 00:44:04 +0100

    WIP

--- index.js
+++ index.js
@@ -1,109 +1,28 @@
-#! /usr/bin/env node
+const delayedPromise = data => new Promise(resolve => resolve(data))
 
-const expand = require('expand-object')
-const jsome = require('jsome')
-const version = require('./package.json').version
-
-jsome.colors = {
-  'num': 'cyan',
-  'str': 'yellow',
-  'bool': 'cyan',
-  'regex': 'blue',
-  'undef': 'grey',
-  'null': 'grey',
-  'attr': 'white',
-  'quot': 'yellow',
-  'punc': 'yellow',
-  'brack': 'cyan'
+// Faking remit for now
+const remit = {
+  request: endpoint => data => delayedPromise(data) 
 }
 
-const program = require('commander')
+const vorpal = require('vorpal')()
+const chalk = vorpal.chalk
+var prettyjson = require('prettyjson')
 
-function collectArgs (val, list) {
-  list.push(val)
-
-  return list
-}
-
-program
-  .version(version)
-  .usage('<target> [options]')
-  .option('-e, --emission', 'Emit')
-  .option('-l, --listen', 'Listen')
-  .option('-k, --keys [str]', 'Key/value pairs from the expand-object package', collectArgs, [])
-  .option('-d, --data [json]', 'Data (overwrites -k)')
-  .option('-f, --file [path]', 'Data file (overwrites -d)')
-  .option('-n, --service-name [name]', 'Service name')
-  .option('-u, --url [url]', 'RabbitMQ URL')
-  .option('-r --raw', 'Raw output')
-  .parse(process.argv)
-
-if (!program.args.length) {
-  program.outputHelp()
-  process.exit(0)
-}
-
-const target = program.args[0]
-let data = {}
-
-if (program.file) {
-  if (!program.file.startsWith('/') && !program.file.startsWith('.')) {
-    program.file = './' + program.file
-  }
-  data = require(program.file)
-} else if (program.data) {
-  data = JSON.parse(program.data)
-} else if (program.keys && program.keys.length) {
-  data = Object.assign(...program.keys.map(expand))
-}
-
-let emit = !!program.emission
-let listen = !!program.listen
-const raw = !!program.raw
-
-if (emit && listen) {
-  console.error('Cannot emit and listen at the same time')
-  process.exit(1)
-}
-
-let serviceName = program.serviceName || 'remit-cli'
-let url = program.url || 'amqp://localhost'
-
-const remit = require('remit')({
-  name: serviceName,
-  url: url
-})
-
-if (emit) {
-  remit.emit(target, data)
-
-  setTimeout(() => {
-    process.exit(0)
-  }, 1000)
-} else if (listen) {
-  console.log(`Listening to ${target} messagse...`)
-
-  remit.listen(target, function (args) {
-    console.log('\n\n', jsome(args))
+vorpal
+  .command('request')
+  .action(async function (args, cb) {
+    remit.request('foo')({foo: {bar: 2}})
+      .then((data) => {
+        this.log(prettyjson.render(data, {
+          keysColor: 'rainbow',
+          dashColor: 'magenta',
+          stringColor: 'white'
+        }))
+        cb()
+      })
   })
-} else {
-  remit.treq(target, data, (err, data) => {
-    if (err) {
-      if (raw) {
-        console.error(JSON.stringify(err))
-      } else {
-        jsome(err)
-      }
-    }
 
-    if (data) {
-      if (raw) {
-        console.log(JSON.stringify(data))
-      } else {
-        jsome(data)
-      }
-    }
-
-    process.exit(0)
-  })
-}
+vorpal
+  .delimiter(chalk.magenta('remit~$'))
+  .show()
